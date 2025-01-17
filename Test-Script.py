6f9420a492ad43a743b6a24c30ff1f27d264ff4a
Mathis <103067982+Mathis912@users.noreply.github.com>
2025-01-17 20:37:16 +0100

    v1

--- Test-Script.py
+++ Test-Script.py
@@ -11,3 +11,11 @@ GPIO.setup(18, GPIO.OUT)
 
 # Initialisieren der Variable "LED" mit dem GPIO Pin 18
 led_pin = 18
+
+# Schleife zum Blinken der LED
+for i in range(0, 10):
+    GPIO.output(led_pin, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(led_pin, GPIO.LOW)
+    time.sleep(1)
+    i += 1
